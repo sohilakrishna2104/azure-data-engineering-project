459c6165ed4603ce87ca18382bb6d49367cd1134
sohilakrishna2104 <113474765+sohilakrishna2104@users.noreply.github.com>
2025-12-01 13:36:53 -0600

    add Silver to Gold Databricks notebook for aggregation

---
+++ databricks/notebooks/silver_to_gold.py
@@ -0,0 +1,11 @@
+# Notebook: Silver to Gold ETL Transformation
+
+# Read cleaned Silver layer data
+df_silver = spark.read.format("delta").load("/mnt/silver/sales")
+
+# Aggregate sales by RegionName
+df_gold = df_silver.groupBy("RegionName").sum("Amount") \
+                   .withColumnRenamed("sum(Amount)", "TotalSales")
+
+# Save aggregated data to Gold layer (Delta format)
+df_gold.write.format("delta").mode("overwrite").save("/mnt/gold/sales_summary")
